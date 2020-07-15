16a2561a0986c89c3f41f1e84cda6b1470614b77
Marcin Szpytma <ma.marcinsz@gmail.com>
2020-07-15 17:14:42 +0200

    Updates

---
+++ precache-manifest.8a56e3b52ca70e3d3b812e1e139d3f9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05c924d74fbb7c37d0c019aa1b206448",
+    "url": "/tasks-list-react/index.html"
+  },
+  {
+    "revision": "3a39bcae4b7292f295bc",
+    "url": "/tasks-list-react/static/css/main.c5eee9eb.chunk.css"
+  },
+  {
+    "revision": "21cccf955065d306d159",
+    "url": "/tasks-list-react/static/js/2.f58a2cb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tasks-list-react/static/js/2.f58a2cb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a39bcae4b7292f295bc",
+    "url": "/tasks-list-react/static/js/main.fc6c9ef4.chunk.js"
+  },
+  {
+    "revision": "fdeb5f49bfb67d33ceb0",
+    "url": "/tasks-list-react/static/js/runtime-main.2f1f908f.js"
+  }
+]);
