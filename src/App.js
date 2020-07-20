86335cf1a3d447ef34bd1a8d82246bf9b101d10f
Marcin Szpytma <ma.marcinsz@gmail.com>
2020-07-20 18:57:28 +0200

    toggleTaskDone interaction

--- src/App.js
+++ src/App.js
@@ -22,6 +22,16 @@ function App() {
     setTasks(tasks => tasks.filter(task => task.id !== id));
   };
 
+  const toggleTaskDone = (id) =>{
+    setTasks(tasks => tasks.map(task =>{
+      if(task.id === id){
+        return{...task, done: !task.done};
+      }
+
+      return task;
+    }))
+  }
+
   return (
       <Container>
         
@@ -38,7 +48,8 @@ function App() {
         <Tasks 
         tasks={tasks} 
         hideDone={hideDone}
-        removeTask = {removeTask} />
+        removeTask = {removeTask}
+        toggleTaskDone = {toggleTaskDone} />
       }
         extraHeaderContent = {
         <Buttons 
--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -2,13 +2,15 @@ import React from "react";
 import "./style.css"
 
 
-const Tasks = ({ tasks, hideDone, removeTask }) => (
+const Tasks = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
   <ul className="tasksList">
     {tasks.map(task => (
       <li
         key={task.id}
         className={`tasksList__item ${task.done && hideDone ? "tasksList__item--hidden" : ""}`}>
-        <button className={`tasksList__button ${task.done ? "tasksList__button--checked" : ""}`}></button>
+        <button className={`tasksList__button ${task.done ? "tasksList__button--checked" : ""}`}
+        onClick = {() => toggleTaskDone(task.id)}
+        ></button>
         <span className={`tasksList__content ${task.done ? "tasksList__content--done" : ""}`} > {task.content}</span>
         <button className="tasksList__button tasksList__button--remove"
         onClick = {() => removeTask(task.id)}
