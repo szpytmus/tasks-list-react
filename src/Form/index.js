c1e873936a761a46b7a107b204a48d778e74f4cb
Marcin Szpytma <ma.marcinsz@gmail.com>
2020-07-20 19:26:45 +0200

    addNewTask interaction

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 
-const Form = () => (
-    <form className="form">
-        <input className="form__input" placeholder="Anything to do?" autofocus />
-        <button className="form__button">Add task</button>
-    </form>
-)
+const Form = ({ addNewTask }) => {
+    const [newTaskContent, setNewTaskContent] = useState("");
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        addNewTask(newTaskContent.trim());
+        setNewTaskContent("");
+    }
+
+
+    return ((
+        <form className="form" onSubmit={onFormSubmit}>
+            <input
+                value={newTaskContent}
+                className="form__input"
+                placeholder="Anything to do?"
+                onChange={({ target }) => setNewTaskContent(target.value)}
+                />
+            <button
+                className="form__button">
+                Add task
+            </button>
+        </form>
+    ))
+}
 
 export default Form;
