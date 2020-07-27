30ae0486aa8860fbda4239ff78198bd784e7e83c
Marcin Szpytma <ma.marcinsz@gmail.com>
2020-07-27 13:20:51 +0200

    adding empty tasks blocked

--- src/Form/index.js
+++ src/Form/index.js
@@ -6,7 +6,13 @@ const Form = ({ addNewTask }) => {
 
     const onFormSubmit = (event) => {
         event.preventDefault();
-        addNewTask(newTaskContent.trim());
+        const newTaskContentTrimmed;
+        
+        if(newTaskContentTrimmed.trim() === ""){
+            return;
+        }
+
+        addNewTask(newTaskContentTrimmed.trim());
         setNewTaskContent("");
     }
 
